68e474b361350b230869e2984a879ba3c5242a5f
Iruoma <Iruomaonyia@outlook.com>
2022-06-30 18:53:26 +0000

    Add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - entry point
+ *
+ * Description: to print a string literal on standard out
+ * Return: returns zero always
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
