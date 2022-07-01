6107810cf50feb8b9c749d997a0a8a5577e4202f
Iruoma <Iruomaonyia@outlook.com>
2022-07-01 18:57:35 +0000

    Add 3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: function to print alphabet in lower and upper case
+ * Return: returns zero always
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	c = 'A';
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
