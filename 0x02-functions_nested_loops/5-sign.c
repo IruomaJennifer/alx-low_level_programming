6f5ca2e394d67c39467fa1a81182ae15de2d189f
Iruoma <Iruomaonyia@outlook.com>
2022-07-05 21:35:09 +0000

    Add 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: an integer whose sign is to be printed
+ *
+ * Return: 1 if it's positive, 0 for zero
+ * -1 for negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
