5467a4a1c3e500724ea1052ef2d8ce60bb1637eb
Iruoma <Iruomaonyia@outlook.com>
2022-07-06 02:29:58 +0000

    Add 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,11 +8,9 @@ int main(void)
 {
 	int sum = 0, i;
 
-	for (i = 3; i < 1024; i++)
+	for (i = 0; i < 1024; i++)
 	{
-		if (i % 3 == 0)
-			sum = sum + i;
-		if (i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 			sum = sum + i;
 	}
 	printf("%d\n", sum);
