06bc5f8f449b6c276e19df675f276268018cfdcd
Iruoma <Iruomaonyia@outlook.com>
2022-07-07 23:57:57 +0000

    Add prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Returns: void
+ */
+
+int main(void)
+{
+	long int num = 612852475143;
+	long int fact = 3;
+
+	while(fact < num)
+	{
+		if (num % fact == 0)
+		{
+			num = num / fact;
+		}
+		fact++;
+	}
+	printf("%ld\n", fact);
+	return (0);
+}
