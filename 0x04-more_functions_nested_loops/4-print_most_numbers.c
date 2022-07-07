e5fc7f884bd8af1c4975c66ec30b80ae5757a156
Iruoma <Iruomaonyia@outlook.com>
2022-07-07 19:41:07 +0000

    Add 4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c == '2' || c == '4')
+		{
+			continue;
+		}
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
