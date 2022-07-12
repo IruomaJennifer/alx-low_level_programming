92c024ac7c6b0569b1acc3816177e3cbfe7276a9
Jennifer <Iruomaonyia@outlook.com>
2022-07-12 23:52:14 +0100

    Add 6-puts2.c after removing wrong code again

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,39 +1,23 @@
 #include "main.h"
 /**
- * _strlen - to get the length of a string
- * @s: a pointer to a character
- * Return: an int
- */
-int _strlen(char *s)
-{
-int len = 0;
-
-while (*s != '\0')
-{
-len++;
-s++;
-}
-return (len);
-}
-/**
  * puts2 - prints every other char
  * @str: pointer to a char variable
  * Return: nada
  */
 void puts2(char *str)
 {
-	int len = _strlen(str);
 	int cnt = 0;
 
-	while (cnt < len)
+	while (cnt >= 0)
 	{
 		if (*str == '\0')
 		{
 			_putchar('\n');
 			break;
 		}
+		if (cnt % 2 == 0)
 		_putchar(*str);
-		str += 2;
+		str ++;
 		cnt++;
 	}
 }
