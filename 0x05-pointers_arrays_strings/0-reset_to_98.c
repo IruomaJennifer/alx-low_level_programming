a9936e3b7b971c3c12873c675190a1a8e2e8dc92
Jennifer <Iruomaonyia@outlook.com>
2022-07-12 20:11:51 +0100

    Add 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - reset a number to 98
+ * @n: pointer to an int
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Start of Pointers, Arrays and Strings
