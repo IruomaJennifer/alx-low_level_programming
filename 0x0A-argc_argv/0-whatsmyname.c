742d6a216bd8d5cc136114593bcacee4e32b66e4
Jennifer <Iruomaonyia@outlook.com>
2022-07-22 18:07:07 +0100

    Add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program to display its name
+ * @argc: an integer
+ * @argv: an array of strings
+ *
+ * Return: an int
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
