3081548b3b39a7f5175085410d6949e2b06d00c7
Jennifer <Iruomaonyia@outlook.com>
2022-07-29 02:00:01 +0100

    Add 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to a type
+ * @old_size: the initial size of the memory location
+ * @new_size: the size of the new memory allocation
+ *
+ * Return: nothing
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *np;
+	unsigned int i;
+	char *pt = (char *)ptr;
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	np = malloc(new_size);
+	if (ptr == NULL)
+		return (np);
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+		{
+			*(np + i) = pt[i];
+		}
+	}
+	else
+	{
+		for (i = 0; i < old_size; i++)
+		{
+			*(np + i) = pt[i];
+		}
+	}
+	free(ptr);
+	return (np);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,7 @@ void *malloc_checked(unsigned int);
 char *string_nconcat(char *, char *, unsigned int);
 void *_calloc(unsigned int, unsigned int);
 int *array_range(int min, int max);
+void *_realloc(void *, unsigned int, unsigned int);
 
 #endif
 
