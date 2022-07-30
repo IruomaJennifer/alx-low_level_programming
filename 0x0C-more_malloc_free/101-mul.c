72feda4ee34bb92ccc62c3bfa6480613917dc9ca
Jennifer <Iruomaonyia@outlook.com>
2022-07-30 02:49:02 +0100

    Add 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,150 +1,89 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
-void printproduct(int *, int);
 /**
- * _strlen - to get the length of a string
- * @s: a pointer to a character
- * Return: an int
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
+int i = 0;
+while (s[i])
+{
+if (s[i] < '0' || s[i] > '9')
+return (0);
+i++;
+}
+return (1);
+}
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-	return (len);
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
 }
-
+return (i);
+}
 /**
- * _isdigit - checks that all string sequence is made up of digits
- * @s: pointer to char
- *
- * Return: an integer if true and 0 if false
+ * errors - handles errors for main
  */
-int _isdigit(char *s)
+void errors(void)
 {
-	int cnt;
-
-	while (*s != '\0')
-	{
-		if (*s >= '0' && *s <= '9')
-			cnt++;
-		else
-			return (0);
-		s++;
-	}
-	return (cnt);
+printf("Error\n");
+exit(98);
 }
-
 /**
-  * initialize - initialize memory location with 0
-  * @keep: pointer to int
-  * @size: size of memory
-  *
-  * Return: nothing
-  */
-void initialize(int *keep, int size)
-{
-	int i;
-
-	for (i = 0; i < size; i++)
-		keep[i] = 0;
-}
-
-/**
- * multiply - multiplies two positive numbers
- * @args: a char pointer to pointer or an array of pointers
- * Return: int
- */
-int multiply(char **args)
-{
-	char *s1 = *(args + 1);
-	char *s2 = *(args + 2);
-	int i, j, digit1, digit2, carry, *keep, len1, len2;
-
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	keep = malloc(sizeof(int) * (len1 + len2));
-	if (keep == 0)
-		return (1);
-	initialize(keep, len1 + len2);
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		digit1 = s1[i] - '0';
-		carry = 0;
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			digit2 = s2[j] - '0';
-			carry += keep[i + j + 1] + (digit1 * digit2);
-			keep[i + j + 1] = carry % 10;
-			carry = carry / 10;
-		}
-		if (carry > 0)
-			keep[i + j + 1] += carry;
-	}
-	printproduct(keep, len1 + len2);
-	free(keep);
-	return (0);
-}
-
-/**
- * printerr - prints error
- */
-void printerr(void)
-{
-	_putchar('E');
-	_putchar('r');
-	_putchar('r');
-	_putchar('o');
-	_putchar('r');
-	_putchar('\n');
-}
-
-/**
- * printproduct - prints a product to the console
- * @keep: result to print, pointer to int
- * @size: size of memory
- *
- * Return: nothing
- */
-void printproduct(int *keep, int size)
-{
-	int i;
-
-	for (i = 0; i < size; i++)
-		_putchar(keep[i] + '0');
-	_putchar('\n');
-}
-
-/**
- * main - entry point
- * @argc: no of parameters
- * @argv: char pointer to pointer
- *
- * Return: int 0 for success and 1 for fail
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
-	int i;
-
-	if (argc < 3)
-	{
-		printerr();
-		exit(98);
-	}
-
-	s1 = argv[1];
-	s2 = argv[2];
-	if (_isdigit(s1) == 0 || _isdigit(s2) == 0)
-	{
-		printerr();
-		exit(98);
-	}
-	i = multiply(argv);
-	return (i);
+char *s1, *s2;
+int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+s1 = argv[1], s2 = argv[2];
+if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+errors();
+len1 = _strlen(s1);
+len2 = _strlen(s2);
+len = len1 + len2 + 1;
+result = malloc(sizeof(int) * len);
+if (!result)
+return (1);
+for (i = 0; i <= len1 + len2; i++)
+result[i] = 0;
+for (len1 = len1 - 1; len1 >= 0; len1--)
+{
+digit1 = s1[len1] - '0';
+carry = 0;
+for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+{
+digit2 = s2[len2] - '0';
+carry += result[len1 + len2 + 1] + (digit1 *digit2);
+result[len1 + len2 + 1] = carry % 10;
+carry /= 10;
 }
-
+if (carry > 0)
+result[len1 + len2 + 1] += carry;
+}
+for (i = 0; i < len - 1; i++)
+{
+if (result[i])
+a = 1;
+if (a)
+_putchar(result[i] + '0');
+}
+if (!a)
+_putchar('0');
+_putchar('\n');
+free(result);
+return (0);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,12 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void *malloc_checked(unsigned int);
-char *string_nconcat(char *, char *, unsigned int);
-void *_calloc(unsigned int, unsigned int);
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-void *_realloc(void *, unsigned int, unsigned int);
-int _putchar(char);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _strlen(char *);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int onlyNumbers(char *c);
+void multiply(char* s1, char* s2);
 
 #endif
-
