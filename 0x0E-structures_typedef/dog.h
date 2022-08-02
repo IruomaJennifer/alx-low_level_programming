680c7eaad0fe940c02a92024024dcecae4bc9c8f
Jennifer <Iruomaonyia@outlook.com>
2022-08-02 04:45:30 +0100

    Add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - properties of a dog
+ * @name: its name
+ * @age: its age
+ * @owner: its owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
