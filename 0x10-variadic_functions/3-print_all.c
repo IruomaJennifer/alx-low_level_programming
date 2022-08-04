e6a9fc2b022b977ad9a070cfcf022f13beb991cb
Jennifer <Iruomaonyia@outlook.com>
2022-08-04 23:28:02 +0100

    To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,22 +2,22 @@
 #include <stdarg.h>
 
 /**
- * print_all - function that prints anything
- * @format: pointer to char
+ * char_checkprint - character check and print
+ * @format: string
+ * @arg: va_list object
+ * @flag: int
  *
- * Return: nothing
+ * Return: void
  */
-void print_all(const char * const format, ...)
+
+void char_checkprint(char format, va_list arg, int *flag)
 {
-	va_list arg;
-	unsigned int i, flag = 0;
 	char *s;
 
-	va_start(arg, format);
-	switch (*format)
+	switch (format)
 	{
 		case 's':
-			flag = 1;
+			*flag = 1;
 			s = va_arg(arg, char *);
 			if (s == NULL)
 			{
@@ -27,20 +27,37 @@ void print_all(const char * const format, ...)
 			printf("%s", s);
 			break;
 		case 'i':
-			flag = 1;
+			*flag = 1;
 			printf("%d", va_arg(arg, int));
 			break;
 		case 'f':
-			flag = 1;
+			*flag = 1;
 			printf("%f", va_arg(arg, double));
 			break;
 		case 'c':
-			flag = 1;
+			*flag = 1;
 			printf("%c", va_arg(arg, int));
 			break;
 		default:
-			flag = 0;
+			*flag = 0;
 	}
+}
+
+
+/**
+ * print_all - function that prints anything
+ * @format: pointer to char
+ *
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	va_list arg;
+	int i = 0, flag = 0;
+
+	va_start(arg, format);
+	if (format[i])
+		char_checkprint(*format, arg, &flag);
 	i = 1;
 	while (format[i])
 	{
@@ -49,33 +66,7 @@ void print_all(const char * const format, ...)
 			printf(", ");
 			break;
 		}
-		switch (*(format + i))
-		{
-			case 's':
-				flag = 1;
-				s = va_arg(arg, char *);
-				if (s == NULL)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", s);
-				break;
-			case 'i':
-				flag = 1;
-				printf("%d", va_arg(arg, int));
-				break;
-			case 'f':
-				flag = 1;
-				printf("%f", va_arg(arg, double));
-				break;
-			case 'c':
-				flag = 1;
-				printf("%c", va_arg(arg, int));
-				break;
-			default:
-				flag = 0;
-		}
+		char_checkprint(*(format + i), arg, &flag);
 		i++;
 	}
 	va_end(arg);
