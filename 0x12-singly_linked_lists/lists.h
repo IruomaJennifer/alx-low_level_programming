49549d61fb7b162eb9bd2959e6cbd55c2f8e9e71
Jennifer <Iruomaonyia@outlook.com>
2022-08-11 21:52:53 +0100

    Add 100-first.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,5 +25,6 @@ int _strlen(const char *);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void first(void);
 
 #endif
