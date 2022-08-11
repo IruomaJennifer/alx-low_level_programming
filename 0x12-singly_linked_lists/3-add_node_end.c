16dcd685cae12bbe802e25bc4854a1a48c0dd91e
Jennifer <Iruomaonyia@outlook.com>
2022-08-11 21:29:31 +0100

    Return temp if *head is null

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,19 +1,4 @@
 #include "lists.h"
-/**
- * _strlen - to get the length of a string
- * @s: a pointer to a character
- * Return: an int
- */
-int _strlen(const char *s)
-{
-	int len = 0;
-
-	while (*(s + len) != '\0' && s != NULL)
-	{
-		len++;
-	}
-	return (len);
-}
 
 /**
  * add_node_end - adds a node at the beginning of a list_t list
@@ -24,26 +9,25 @@ int _strlen(const char *s)
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *temp = NULL, *current, *prev;
+	list_t *temp = NULL, *current;
 
 	if (head != NULL)
 	{
 		current = *head;
-		while (current != NULL)
-		{
-			prev = current;
-			current = current->next;
-		}
 		temp = malloc(sizeof(list_t));
 		if (temp == NULL)
 			return (NULL);
-		temp->len = _strlen(str);
+		temp->len = strlen(str);
 		temp->str = strdup(str);
-		temp->next = current;
+		temp->next = NULL;
 		if (*head == NULL)
+		{
 			*head = temp;
-		prev->next = temp;
-		return (temp);
+			return (temp);
+		}
+		while (current->next != NULL)
+			current = current->next;
+		current->next = temp;
 	}
 	return (temp);
 }
