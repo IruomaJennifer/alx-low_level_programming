057ac38fec7b0814978f1e1aca5736673c08440c
Jennifer <Iruomaonyia@outlook.com>
2022-08-23 04:20:23 +0100

    0-read_file correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,40 @@
 #include "main.h"
-
 /**
- * read_textfile - function to read and print the contents of a text file
- * to stdout
- * @filename: the filename
- * @letters: the number of letters
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
  *
- * Return: number of letters printed
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t w, r;
+	 ssize_t o, r, w;
 	char *buffer;
 
+	if (filename == NULL)
+		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
-
-	if (!buffer || !filename)
+	if (buffer ==  NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-
-	r = read(fd, buffer, letters);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
-	if (w == -1 || r == -1)
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
+
+	free(buffer);
+	close(o);
 
 	return (w);
+
 }
+
